3525da5460648b3597af02fc2b3410f39b2e8f88
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-28 15:39:09 +0200

    Implement Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,47 @@
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 function Contact() {
+  const links = [
+    {
+      name: "Email",
+      icon: <EmailIcon className="contact-icon" />,
+      url: "mailto:rautela4488@gmail.com",
+    },
+    {
+      name: "Phone",
+      icon: <LocalPhoneIcon className="contact-icon" />,
+      url: "tel:+48577855970",
+    },
+    {
+      name: "GitHub",
+      icon: <GitHubIcon className="contact-icon" />,
+      url: "https://github.com/aayushrautela",
+    },
+    {
+      name: "LinkedIn",
+      icon: <LinkedInIcon className="contact-icon" />,
+      url: "https://www.linkedin.com/in/aayush-rautela-50471a240/",
+    },
+  ];
+
   return (
-    <section>
-      <h3>Contact</h3>
+    <section className="contact">
+      <h2>Contact</h2>
+      <div className="contact-links">
+        {links.map((link) => (
+          <a
+            href={link.url}
+            key={link.name}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="contact-link">{link.icon}</span>
+          </a>
+        ))}
+      </div>
     </section>
   );
 }
