53bd1b17f177081077dadd1a8ac08074a6a2eaca
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-28 13:00:27 +0200

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div>
+      <Projects />
+      <AboutMe />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,5 @@
+function AboutMe() {
+  return <div>About me</div>;
+}
+
+export default AboutMe;
