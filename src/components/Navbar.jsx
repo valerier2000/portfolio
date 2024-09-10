b1ec31fbcda9b7411c8592b2ac06d01299bcb3f2
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-09-10 19:19:24 +0200

    Ensure page doesn't reload on logo click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import "./Navbar.css";
 
+const handleLogoClick = (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <a href="/" className="logo">
+        <a href="/" className="logo" onClick={handleLogoClick}>
           Aayush Rautela
         </a>
       </div>
