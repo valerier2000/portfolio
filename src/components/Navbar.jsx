7236ca784e3bc5987e977bae8adf241fad1c502a
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-09-10 19:17:11 +0200

    Revert "Ensure page doesn't reload on logo click"
    This reverts commit bf7d3188405fead07a4e61d7e9fe321bbf25734a.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
 import "./Navbar.css";
 
-const handleLogoClick = (e) => {
-  e.preventDefault();
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <a href="/" className="logo" onClick={handleLogoClick}>
+        <a href="/" className="logo">
           Aayush Rautela
         </a>
       </div>
