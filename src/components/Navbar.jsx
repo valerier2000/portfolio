41cb8222c7c89eb0084d467032cf42fae6c6cc63
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-05 22:05:32 +0100

    Add animated line under nav-link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,16 +16,24 @@ function Navbar() {
       <div className="navbar-right">
         <ul className="nav-links">
           <li>
-            <a href="#about-me">About me</a>
+            <a href="#about-me" className="nav-link">
+              About me
+            </a>
           </li>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" className="nav-link">
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" className="nav-link">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
           </li>
         </ul>
       </div>
