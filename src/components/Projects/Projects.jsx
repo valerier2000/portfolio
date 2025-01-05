fe31695d6be978f716fa686fb34ef49fc914ebce
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-05 18:41:55 +0100

    Change projects to grid

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,25 @@
-import Carousel from "./Carousel";
-import project3 from "../../assets/p3.jpg";
+import project3 from "../../assets/Valeriia.jpg";
 import "./Projects.css";
 
-const slides = [
+const projects = [
   {
-    url: project3,
+    img: project3,
     name: "OnTrack",
+    url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web application designed to streamline project management across industries by replacing traditional methods with a digital Kanban board. OnTrack enables users to add projects and tasks, move tasks between columns (To Do, Doing, Done),and utilize drag-and-drop functionality for easy task management",
   },
   {
-    url: project3,
+    img: project3,
     name: "7 Wonders Table Calculator",
+    url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web-based calculator designed to simplify score tracking for the board game 7 Wonders. The application allows players to input scores across various categories, calculates totals dynamically, and provides a user-friendly interface for managing game scoring.",
   },
   {
-    url: project3,
+    img: project3,
     name: "Project 3",
+    url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quibusdam voluptatem quia laborum rem, distinctio incidunt facere eius mollitia enim odio optio aliquid et nesciunt dignissimos nam. Enim, assumenda rem",
   },
@@ -26,8 +28,19 @@ const slides = [
 function Projects() {
   return (
     <section className="projects" id="projects">
-      {/* <h2>Projects</h2> */}
-      <Carousel slides={slides} />
+      <h2>Projects</h2>
+      {/* <Carousel slides={slides} /> */}
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <div className="project" key={project.name}>
+            <img src={project.img} alt={`Project ${project.name}`} />
+            <h3>
+              <a href={project.url}>{project.name}</a>
+            </h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
