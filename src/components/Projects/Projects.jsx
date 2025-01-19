966afb47e1a2ea03a53f6892fd57a83eeed73989
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-19 16:39:25 +0100

    Add OnTrack image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,23 @@
-import project3 from "../../assets/Valeriia.jpg";
+import OnTrack from "../../assets/projects/OnTrackLogo.png";
 import "./Projects.css";
 
 const projects = [
   {
-    img: project3,
+    img: OnTrack,
     name: "OnTrack",
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web application designed to streamline project management across industries by replacing traditional methods with a digital Kanban board. OnTrack enables users to add projects and tasks, move tasks between columns (To Do, Doing, Done),and utilize drag-and-drop functionality for easy task management",
   },
   {
-    img: project3,
+    img: OnTrack,
     name: "7 Wonders Table Calculator",
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web-based calculator designed to simplify score tracking for the board game 7 Wonders. The application allows players to input scores across various categories, calculates totals dynamically, and provides a user-friendly interface for managing game scoring.",
   },
   {
-    img: project3,
+    img: OnTrack,
     name: "Project 3",
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
