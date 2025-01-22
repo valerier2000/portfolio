8beabf6fff9d9076aec24c494a81feb256e97ad0
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-22 13:31:53 +0100

    Dynamically change the image on hover for each project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,27 @@
+import { useState } from "react";
 import OnTrack from "../../assets/projects/OnTrackLogo.png";
+import OnTrackTasks from "../../assets/projects/OnTrack-Tasks.png";
 import "./Projects.css";
 
 const projects = [
   {
-    img: OnTrack,
-    name: "OnTrack",
+    defaultImg: OnTrack,
+    hoverImg: OnTrackTasks,
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web application designed to streamline project management across industries by replacing traditional methods with a digital Kanban board. OnTrack enables users to add projects and tasks, move tasks between columns (To Do, Doing, Done),and utilize drag-and-drop functionality for easy task management",
   },
   {
-    img: OnTrack,
+    defaultImg: OnTrack,
+    hoverImg: OnTrackTasks,
     name: "7 Wonders Table Calculator",
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
       "A web-based calculator designed to simplify score tracking for the board game 7 Wonders. The application allows players to input scores across various categories, calculates totals dynamically, and provides a user-friendly interface for managing game scoring.",
   },
   {
-    img: OnTrack,
+    defaultImg: OnTrack,
+    hoverImg: OnTrackTasks,
     name: "Project 3",
     url: "https://adrgri.github.io/OnTrack.Frontend.React/",
     description:
@@ -26,15 +30,29 @@ const projects = [
 ];
 
 function Projects() {
+  const [hoveredProject, setHoveredProject] = useState(null);
+
   return (
     <section className="projects" id="projects">
       <h2>Projects</h2>
-      {/* <Carousel slides={slides} /> */}
+
       <div className="projects-grid">
-        {projects.map((project) => (
-          <div className="project" key={project.name}>
+        {projects.map((project, index) => (
+          <div
+            className="project"
+            key={project.name}
+            onMouseEnter={() => setHoveredProject(index)}
+            onMouseLeave={() => setHoveredProject(null)}
+          >
             <a href={project.url} target="_blank" rel="noopener noreferrer">
-              <img src={project.img} alt={`Project ${project.name}`} />
+              <img
+                src={
+                  hoveredProject === index
+                    ? project.hoverImg
+                    : project.defaultImg
+                }
+                alt={`Project ${project.name}`}
+              />
               <h3>{project.name}</h3>
               <p>{project.description}</p>
             </a>
