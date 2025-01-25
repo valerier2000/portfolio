4fdd315b3c17d513cde3a320d825c39d451a88e4
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-25 15:57:02 +0100

    Move links array outside component for better performance

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,30 +4,30 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "./Contact.css";
 
-function Contact() {
-  const links = [
-    {
-      name: "Email",
-      icon: <EmailIcon className="contact-icon" />,
-      url: "mailto:valeriiarohatynska@gmail.com",
-    },
-    {
-      name: "Phone",
-      icon: <LocalPhoneIcon className="contact-icon" />,
-      url: "tel:+48575246082",
-    },
-    {
-      name: "GitHub",
-      icon: <GitHubIcon className="contact-icon" />,
-      url: "https://github.com/valerier2000",
-    },
-    {
-      name: "LinkedIn",
-      icon: <LinkedInIcon className="contact-icon" />,
-      url: "https://www.linkedin.com/in/valerier2000/",
-    },
-  ];
+const links = [
+  {
+    name: "Email",
+    icon: <EmailIcon className="contact-icon" />,
+    url: "mailto:valeriiarohatynska@gmail.com",
+  },
+  {
+    name: "Phone",
+    icon: <LocalPhoneIcon className="contact-icon" />,
+    url: "tel:+48575246082",
+  },
+  {
+    name: "GitHub",
+    icon: <GitHubIcon className="contact-icon" />,
+    url: "https://github.com/valerier2000",
+  },
+  {
+    name: "LinkedIn",
+    icon: <LinkedInIcon className="contact-icon" />,
+    url: "https://www.linkedin.com/in/valerier2000/",
+  },
+];
 
+function Contact() {
   return (
     <section className="contact" id="contact">
       <h2>Contact</h2>
